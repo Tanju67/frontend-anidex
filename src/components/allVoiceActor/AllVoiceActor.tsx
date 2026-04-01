3c4edd48997532f2553db8fff09cb5e585e1b610
Tanju Özer <tanjuozer64@gmail.com>
2026-04-02 00:30:16 +0200

    create all voice actor page

---
+++ src/components/allVoiceActor/AllVoiceActor.tsx
@@ -0,0 +1,52 @@
+import { useCallback, useState } from "react";
+import { useParams } from "react-router-dom";
+import { useGetCharactersByAnimeIdQuery } from "../../shared/api/animeApi";
+import { useInView } from "../../shared/hooks/useInView";
+import { useSafeQuery } from "../../shared/hooks/useSafeQuery";
+import { CharactersSchema } from "../../shared/schemas/animeSchema";
+import CharacterContentSkeleton from "../../shared/UIElements/skeleton/CharacterContentSkeleton";
+import Character from "../../shared/UIElements/character/Character";
+
+function AllVoiceActor() {
+  const { animeId } = useParams();
+  const [visibleCount, setVisibleCount] = useState(12);
+
+  const query = useGetCharactersByAnimeIdQuery(animeId!);
+
+  const { data, isLoading, isError } = useSafeQuery({
+    data: query.data,
+    isLoading: query.isLoading,
+    schema: CharactersSchema,
+  });
+
+  const loadMore = useCallback(() => {
+    if (!data) return;
+
+    setVisibleCount((prev) => Math.min(prev + 12, data.length));
+  }, [data]);
+
+  const { ref } = useInView({
+    onEnter: loadMore,
+    triggerOnce: false,
+  });
+
+  const visibleCharacters = data?.slice(0, visibleCount) ?? [];
+
+  if (isLoading) return <CharacterContentSkeleton isCharacter={false} />;
+  if (isError || !data?.length)
+    return <div className="text-center opacity-60">No data found</div>;
+  return (
+    <div className="max-w-400rem mx-auto">
+      <Character
+        isCharacter={false}
+        title="All Voice Actors"
+        data={visibleCharacters}
+        isBack={true}
+      />
+
+      {visibleCount < data.length && <div ref={ref} className="h-20" />}
+    </div>
+  );
+}
+
+export default AllVoiceActor;
